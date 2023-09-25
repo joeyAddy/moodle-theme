2cdca6ef560aa788988469e010de20409adad017
GitGoodness <chatwithjohnjoseph@gmail.com>
2023-09-24 21:26:38 -0700

    first commit

---
+++ moodle_data/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = '6PhrpHNzrZwhsC24hc13F9RIAjgeVRJklocalhost';
+$CFG->bootstraphash = '925aa181070fc1f8cd608a669453e30e';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
